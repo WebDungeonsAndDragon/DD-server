a9f2620cead6f2a7d7c6259c2b4d9a979ac54075
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-02-27 18:44:06 -0500

    added createRoom call

--- src/controller/GameStateController.controller.js
+++ src/controller/GameStateController.controller.js
@@ -3,6 +3,11 @@ const { Server } = require("socket.io");
 const express = require("express");
 const router = express.Router();
 
+const Room = require("../model/Room.model");
+const Player = require("../model/Player.model");
+
+const rooms = {};
+
 const io = new Server({
   cors: {
     origin: "http://localhost:5173",
@@ -11,6 +16,10 @@ const io = new Server({
 
 io.on("connection", (socket) => {
   console.log("new user connected!");
+
+  socket.on("createRoom", ({ roomId, hostName }) => {
+    rooms[roomId] = new Room(roomId, new Player(socket.id, hostName));
+  });
 
   socket.on("example", ({ name, room }, callback) => {
     // const { error, user } = addUser({ id: socket.id, name, room });
