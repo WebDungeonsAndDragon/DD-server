166a5fe227bd863c870ca58fff8a2db5c1dadbd5
Matthew J Bender <mbender9@gatech.edu>
2024-02-27 19:13:35 -0500

    merged

--- src/controller/GameStateController.controller.js
+++ src/controller/GameStateController.controller.js
@@ -3,6 +3,12 @@ const { Server } = require("socket.io");
 const express = require("express");
 const router = express.Router();
 
+const Room = require("../model/Room.model");
+const Player = require("../model/Player.model");
+
+const rooms = {};
+
+
 const io = new Server({
   cors: {
     origin: "http://localhost:5173",
@@ -11,7 +17,9 @@ const io = new Server({
 
 io.on("connection", (socket) => {
   console.log("new user connected!");
-
+  socket.on("createRoom", ({ roomId, hostName }) => {
+    rooms[roomId] = new Room(roomId, new Player(socket.id, hostName));
+  });
   socket.on("join", ({ name, room }, callback) => {
     // const { error, user } = addUser({ id: socket.id, name, room });
     // if (error) return callback(error);
