ded7337a067be97717190719c5dffb529b27b31a
erika-tay <147895704+erika-tay@users.noreply.github.com>
2024-02-29 23:37:48 -0500

    completed endRound & leaveGame

--- src/controller/GameStateController.controller.js
+++ src/controller/GameStateController.controller.js
@@ -3,6 +3,11 @@ const { Server } = require("socket.io");
 const express = require("express");
 const router = express.Router();
 
+const Room = require("../model/Room.model");
+const Player = require("../model/Player.model");
+
+const rooms = {};
+
 const io = new Server({
   cors: {
     origin: "http://localhost:5173",
@@ -11,6 +16,10 @@ const io = new Server({
 
 io.on("connection", (socket) => {
   console.log("new user connected!");
+
+  socket.on("createRoom", ({ roomId, hostName }) => {
+    rooms[roomId] = new Room(roomId, new Player(socket.id, hostName));
+  });
 
   socket.on("join", ({ name, room }, callback) => {
     // const { error, user } = addUser({ id: socket.id, name, room });
@@ -21,8 +30,58 @@ io.on("connection", (socket) => {
     // welcome to room ${user.room}.`,
     // });
   });
+
+  // NEXT TURN 
+
+  // Client 
+  // socket.emit("next-turn", ({currentPlayerAction, currentPlayer, roomId});
+
+  // Server 
+  socket.on("next-turn", ({currentPlayerAction, currentPlayer, roomId}) => {
+    // socket.emit("next-turn-to-client", currentPlayerTurn, newPrompt); 
+    // increase player index 
+
+  });
+
+  // Client 
+  // socket.on("next-turn-to-client", (currentPlayerTurn, newPrompt) => {
+  // 
+  // });
+
+  // END ROUND
+
+  function endRound(roomId) {
+    rooms[roomId].updateRoundNumber();
+    socket.emit("end-round", {
+      roundNumber: rooms[roomId].currentRoundNumber,
+    });
+  }
+
+
+  // Client
+  socket.on("end-round-message", ({roundNumber}) => {
+    // display roundnumber
+    // socket.emit success? 
+  });
+
+  // Display roundNumber? need roomId? add roundNumber to room?
+
+  // LEAVE GAME
+
+  // Client
+  // socket.emit("leave-game", {playerId, roomId});
+
+  // Server
+  socket.on("leave-game", ({playerThatLeft, roomId}) => {
+    rooms[roomId].removePlayer(playerThatLeft)
+    console.log(playerThatLeft + " has left.")
+    // playerId, not playerName
+  });
+
+  
+  
 });
 
-io.listen(4000);
+io.listen(8000);
 
 module.exports = router;
--- src/model/Room.model.js
+++ src/model/Room.model.js
@@ -2,6 +2,8 @@ class Room {
   constructor(id, host) {
     this.id = id;
     this.players = [host];
+    this.totalRounds = -1;
+    this.currentRoundNumber = -1; 
   }
 
   addPlayer(player) {
@@ -11,6 +13,18 @@ class Room {
   removePlayer(player) {
     this.players = this.players.filter((p) => player.id !== p.id);
   }
+
+  startGame(totalRounds) {
+    this.totalRounds = totalRounds;
+    this.roundNumber = 0;
+    this.currentPlayerTurn = 0;
+  }
+
+  updateRoundNumber() {
+    this.roundNumber += 1;
+  }
+
+
 }
 
 module.exports = Room;
